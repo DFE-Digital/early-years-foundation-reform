e166da5d2a2dbbc24394e4293ec73dd198cbad16
Guy Roberts <groberts2001@hotmail.com>
2021-01-21 18:49:16 +0000

    Fixed damaged syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,53 +13,52 @@ if !User.exists?(email: 'dev@hfeyp.com')
   end
 end
 
-
 content_pages = [
     {
         title: 'Communication and language',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'communications-and-language',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
-
+    {
         title: 'Physical development',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'physical-development',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
     {
         title: 'Personal, social and emotional development',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'personal-social-emotional-development',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
     {
         title: 'Literacy',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'literacy',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
     {
         title: 'Mathematics',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'mathematics',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
     {
         title: 'Understanding the world',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'understanding-the-world',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
     },
     {
         title: 'Expressive arts and design',
-        #subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
+        subtitle: 'Information, guidance, practical support and training for delivering the Early Years foundation Stage (EYFS)',
         slug: 'expressive-arts-and-design',
         markdown: '#None yet',
         seo: 'Early-Years-Foundation-Stage'
