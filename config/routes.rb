c01d4a4e79b3a6b03f1e232838ff34960a3bbdf2
Tony To <tonyto85@gmail.com>
2021-01-21 14:10:13 +0000

    Clean lauout for signing page and requre auth for content controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
   resources :content_pages
 
   root 'content_pages#index'
