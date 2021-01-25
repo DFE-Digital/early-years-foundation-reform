ea6533daeccae799d888788130b58f433ec80dec
Marc Rice <marcrice09@icloud.com>
2021-01-25 17:46:59 +0000

    trait with_avatar

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,5 @@
+include ActionDispatch::TestProcess
+
 FactoryBot.define do
   factory :user do
     email { "test@test.com" }
@@ -8,5 +10,9 @@ end
 FactoryBot.define do
   factory :content_asset do
     title { "Sample Content Asset" }
+
+    trait :with_avatar do
+      avatar { fixture_file_upload(Rails.root.join('spec/fixtures/sample.jpeg'), 'image/jpeg') }
+    end
   end
 end
