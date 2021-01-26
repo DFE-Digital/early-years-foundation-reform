95d1190ec56d3695307ee5cec086810dcab018e0
Marc Rice <marcrice12@gmail.com>
2021-01-26 14:28:53 +0000

    refactor to factory

--- spec/views/content_assets/index.html.erb_spec.rb
+++ spec/views/content_assets/index.html.erb_spec.rb
@@ -3,21 +3,8 @@ require 'rails_helper'
 RSpec.describe "content_assets/index", type: :view do
   before(:each) do
 
-    @content_asset = ContentAsset.new(title: "Title")
-    @content_asset.avatar.attach(
-      io: File.open(Rails.root.join("spec/fixtures/sample.jpeg")),
-      filename: 'sample.jpeg',
-      content_type: 'image/jpeg',
-    )
-    @content_asset.save!
-
-    @content_asset2 = ContentAsset.new(title: "Title")
-    @content_asset2.avatar.attach(
-      io: File.open(Rails.root.join("spec/fixtures/sample.jpeg")),
-      filename: 'sample.jpeg',
-      content_type: 'image/jpeg',
-    )
-    @content_asset2.save!
+    @content_asset = FactoryBot.create(:content_asset)
+    @content_asset2 = FactoryBot.create(:content_asset)
 
     assign(:content_assets, [@content_asset, @content_asset2])
   end
