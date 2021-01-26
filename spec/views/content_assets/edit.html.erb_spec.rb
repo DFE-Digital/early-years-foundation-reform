c5b03a33dbae39de526f859999a282e874e50a69
Marc Rice <marcrice12@gmail.com>
2021-01-26 16:44:02 +0000

    using mock for asset

--- spec/views/content_assets/edit.html.erb_spec.rb
+++ spec/views/content_assets/edit.html.erb_spec.rb
@@ -2,15 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "content_assets/edit", type: :view do
   before(:each) do
-
-    @content_asset = ContentAsset.new(title: "test title")
-    @content_asset.avatar.attach(
-      io: File.open(Rails.root.join("spec/fixtures/sample.jpeg")),
-      filename: 'sample.jpeg',
-      content_type: 'image/jpeg',
-    )
-    @content_asset.save!
-
+    @content_asset = FactoryBot.create(:content_asset)
   end
 
   it "renders the edit content_asset form" do
