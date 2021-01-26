b40a9b83563e0971dbd44ed654c02b573fb9a53e
Marc Rice <marcrice09@icloud.com>
2021-01-26 11:43:26 +0000

    check for attrs

--- spec/models/content_asset_spec.rb
+++ spec/models/content_asset_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ContentAsset, type: :model do
-
-  describe "with a valid file" do
-
-    #let(:content_asset) { create :content_asset, :with_avatar }
-
+  describe "with an attached file" do
     before(:each) do
-      @content_asset = ContentAsset.new(title: "test title")
-      @content_asset.avatar.attach(
-        io: File.open(Rails.root.join("spec/fixtures/sample.jpeg")),
-        filename: 'sample.jpeg',
-        content_type: 'image/jpeg',
-      )
-      @content_asset.save!
+      @content_asset = FactoryBot.create(:content_asset)
     end
 
     it "has a file attached" do
       expect(@content_asset.avatar).to be_attached
     end
-
   end
 
   describe "required attributes" do
-
-    it "requires a title and a avatar(file)" do
+    it "requires a title" do
       record = ContentAsset.new
-      record.title = ""
       record.validate
+      puts "content asset attributes hash: #{record.errors.inspect}"
       expect(record.errors[:title]).to include("can't be blank")
-
+    end
+    it "requires an asset to be uploaded" do
+      record = ContentAsset.new
+      record.validate
+      puts "content asset attributes hash: #{record.errors.inspect}"
+      expect(record.errors[:avatar]).to include("can't be blank")
     end
   end
-
 end
-
