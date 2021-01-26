2e6aef37372185b3826d81ebfc80a91fb7bebe08
Tony To <tonyto85@gmail.com>
2021-01-26 13:54:15 +0000

    Rubocop autofix
    Fixed invalid equality checker

--- app/controllers/content_assets_controller.rb
+++ app/controllers/content_assets_controller.rb
@@ -1,6 +1,6 @@
 class ContentAssetsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_content_asset, only: [:show, :edit, :update, :destroy]
+  before_action :set_content_asset, only: %i[show edit update destroy]
 
   # GET /content_assets
   def index
@@ -8,8 +8,7 @@ class ContentAssetsController < ApplicationController
   end
 
   # GET /content_assets/1
-  def show
-  end
+  def show; end
 
   # GET /content_assets/new
   def new
@@ -17,15 +16,14 @@ class ContentAssetsController < ApplicationController
   end
 
   # GET /content_assets/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /content_assets
   def create
     @content_asset = ContentAsset.new(content_asset_params)
 
     if @content_asset.save
-      redirect_to @content_asset, notice: 'Content asset was successfully created.'
+      redirect_to @content_asset, notice: "Content asset was successfully created."
     else
       render :new
     end
@@ -34,7 +32,7 @@ class ContentAssetsController < ApplicationController
   # PATCH/PUT /content_assets/1
   def update
     if @content_asset.update(content_asset_params)
-      redirect_to @content_asset, notice: 'Content asset was successfully updated.'
+      redirect_to @content_asset, notice: "Content asset was successfully updated."
     else
       render :edit
     end
@@ -42,8 +40,8 @@ class ContentAssetsController < ApplicationController
 
   # DELETE /content_assets/1
   def destroy
-    @content_asset.destroy
-    redirect_to content_assets_url, notice: 'Content asset was successfully destroyed.'
+    @content_asset.destroy!
+    redirect_to content_assets_url, notice: "Content asset was successfully destroyed."
   end
 
 private
@@ -57,5 +55,4 @@ private
   def content_asset_params
     params.require(:content_asset).permit(:title, :avatar)
   end
-
 end
--- spec/views/content_assets/show.html.erb_spec.rb
+++ spec/views/content_assets/show.html.erb_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "content_assets/show", type: :view do
   before(:each) do
     @content_asset = assign(:content_asset, ContentAsset.create!(
-      title: "Title"
-    ))
+                                              title: "Title",
+                                            ))
   end
 
   it "renders attributes in <p>" do
