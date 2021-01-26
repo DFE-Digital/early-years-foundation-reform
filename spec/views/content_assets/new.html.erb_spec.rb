d38ca789ce5a815c1b2e5ba44ef1cf53bc712240
Marc Rice <marcrice09@icloud.com>
2021-01-26 11:44:10 +0000

    refactor to mock

--- spec/views/content_assets/new.html.erb_spec.rb
+++ spec/views/content_assets/new.html.erb_spec.rb
@@ -2,16 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "content_assets/new", type: :view do
   before(:each) do
-    assign(:content_asset, ContentAsset.new(
-      title: "MyString"
-    ))
+    assign(:content_asset, FactoryBot.build(:content_asset))
   end
 
   it "renders new content_asset form" do
     render
 
     assert_select "form[action=?][method=?]", content_assets_path, "post" do
-
       assert_select "input[name=?]", "content_asset[title]"
     end
   end
--- spec/views/content_assets/show.html.erb_spec.rb
+++ spec/views/content_assets/show.html.erb_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "content_assets/show", type: :view do
   before(:each) do
-    @content_asset = ContentAsset.new(title: "Title")
-    @content_asset.avatar.attach(
-      io: File.open(Rails.root.join("spec/fixtures/sample.jpeg")),
-      filename: 'sample.jpeg',
-      content_type: 'image/jpeg',
-    )
-    @content_asset.save!
+    @content_asset = FactoryBot.create(:content_asset)
   end
 
   it "renders attributes in <p>" do
