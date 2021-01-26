b425b54ee3794f5eedade3d9da5a1146efafd01d
Marc Rice <marcrice09@icloud.com>
2021-01-26 09:52:27 +0000

    test webpacker loading js

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -5,6 +5,10 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { initAll } from 'govuk-frontend';
 
+import { hello } from "custom/markdown-preview";
+
 Rails.start();
 Turbolinks.start();
 initAll();
+
+hello();
