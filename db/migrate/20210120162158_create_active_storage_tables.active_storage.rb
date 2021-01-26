020a859ec7edec9539a70ad30a132aa18640343f
Tony To <tonyto85@gmail.com>
2021-01-26 14:35:01 +0000

    Adding timestamp fields to active_storage tables

--- db/migrate/20210120162158_create_active_storage_tables.active_storage.rb
+++ db/migrate/20210120162158_create_active_storage_tables.active_storage.rb
@@ -9,7 +9,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.string   :service_name, null: false
       t.bigint   :byte_size,    null: false
       t.string   :checksum,     null: false
-      t.datetime :created_at,   null: false
+      t.timestamps
 
       t.index [:key], unique: true
     end
@@ -19,7 +19,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.references :record,   null: false, polymorphic: true, index: false
       t.references :blob,     null: false
 
-      t.datetime :created_at, null: false
+      t.timestamps
 
       t.index %i[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
@@ -31,6 +31,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
 
       t.index %i[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
+      t.timestamps
     end
   end
 end
