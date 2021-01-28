684bb1687e3e5f08e56920e725de70b170b81e13
marc <marcrice12@gmail.com>
2021-01-28 13:52:17 +0000

    has explicit subject

--- spec/models/content_asset_spec.rb
+++ spec/models/content_asset_spec.rb
@@ -2,29 +2,28 @@ require "rails_helper"
 
 RSpec.describe ContentAsset, type: :model do
   describe "with an attached file" do
+    subject { ContentAsset.new }
+
     before(:each) do
-      @content_asset = ContentAsset.new(title: "Title")
-      @content_asset.avatar.attach(io: File.open("spec/fixtures/sample.jpeg"), filename: "sample.jpeg", content_type: "image/jpeg")
-      @content_asset.save!
+      subject.title = "Title"
+      subject.avatar.attach(io: File.open("spec/fixtures/sample.jpeg"), filename: "sample.jpeg", content_type: "image/jpeg")
+      subject.save!
     end
 
     it "has a file attached" do
-      expect(@content_asset.avatar).to be_attached
+      expect(subject.avatar).to be_attached
     end
   end
 
   describe "required attributes" do
+    subject { ContentAsset.new }
     it "requires a title" do
-      record = ContentAsset.new
-      record.validate
-      puts "content asset attributes hash: #{record.errors.inspect}"
-      expect(record.errors[:title]).to include("can't be blank")
+      subject.validate
+      expect(subject.errors[:title]).to include("can't be blank")
     end
     it "requires an asset to be uploaded" do
-      record = ContentAsset.new
-      record.validate
-      puts "content asset attributes hash: #{record.errors.inspect}"
-      expect(record.errors[:avatar]).to include("can't be blank")
+      subject.validate
+      expect(subject.errors[:avatar]).to include("can't be blank")
     end
   end
 end
