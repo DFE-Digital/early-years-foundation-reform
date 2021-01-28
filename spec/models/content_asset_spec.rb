dcdffa1f873e01c7d80889c319b4f3c3995de386
marcrice12 <78028419+marcrice12@users.noreply.github.com>
2021-01-28 16:14:31 +0000

    Testcoverage - see previous repo (#3)
    * fix issue with rails preventing form redirection
    
    * add sample jpeg to fixtures
    
    * addidional paths
    
    * assets tests
    
    * trait with_avatar
    
    * assets specs
    
    * ignore mac hidden files
    
    * remove trait
    
    * factory with upload for asset
    
    * check for attrs
    
    * refactor to mock
    
    * refactor to factory
    
    * using mock for asset
    
    * indentation
    
    * asset model spec instaniates obj not factory
    
    * has explicit subject
    
    * connecting to docker instance
    
    Co-authored-by: Marc Rice <marcrice09@icloud.com>
    Co-authored-by: Guy Roberts <groberts2001@hotmail.com>
    Co-authored-by: Tony To <tonyto85@gmail.com>

--- spec/models/content_asset_spec.rb
+++ spec/models/content_asset_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe ContentAsset, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with an attached file" do
+    subject { ContentAsset.new }
+
+    before(:each) do
+      subject.title = "Title"
+      subject.avatar.attach(io: File.open("spec/fixtures/sample.jpeg"), filename: "sample.jpeg", content_type: "image/jpeg")
+      subject.save!
+    end
+
+    it "has a file attached" do
+      expect(subject.avatar).to be_attached
+    end
+  end
+
+  describe "required attributes" do
+    subject { ContentAsset.new }
+    it "requires a title" do
+      subject.validate
+      expect(subject.errors[:title]).to include("can't be blank")
+    end
+    it "requires an asset to be uploaded" do
+      subject.validate
+      expect(subject.errors[:avatar]).to include("can't be blank")
+    end
+  end
 end
