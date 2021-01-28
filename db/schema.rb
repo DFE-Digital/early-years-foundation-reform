baa18856da333d578a0fc5e7833b7d20881cd97e
guy-roberts-dfe <groberts2001@hotmail.com>
2021-01-28 09:39:47 +0000

    Create a tree of ContentPages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_162158) do
+ActiveRecord::Schema.define(version: 2021_01_27_144559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_01_20_162158) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "subtitle"
+    t.integer "parent_id"
+    t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
