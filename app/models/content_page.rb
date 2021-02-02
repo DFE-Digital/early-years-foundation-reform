49a6a1cf5bda79233348bf2592d683ef177a73e2
Guy Roberts <groberts2001@hotmail.com>
2021-02-02 11:49:48 +0000

    Better page index (#7)
    * Testing if I can push to this new repo
    
    * Create a tree of ContentPages
    
    * Create a tree of ContentPages
    
    * Testing if I can push to this new repo
    
    * Rebasing
    
    * ebase
    
    * Smaller titles
    
    * Rebase
    
    * No schema for demo
    
    * Temporary refresh button
    
    * Test for child pages
    Changed title
    
    * Robocop
    
    * Better styling for textareas

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -1,7 +1,9 @@
 class ContentPage < ApplicationRecord
+  acts_as_tree
+  scope :top_level, -> { where("parent_id IS NULL") }
+
   validates :title, presence: true, uniqueness: true
   validates :subtitle, presence: true
-  validates :slug, presence: true, uniqueness: true
   validates :markdown, presence: true
   validates :seo, presence: true
 end
