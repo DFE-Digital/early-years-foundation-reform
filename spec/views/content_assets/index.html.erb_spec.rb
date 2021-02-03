ac2555d27a70220865d2d31e2a6e8adb7b349b94
marc <marcrice12@gmail.com>
2021-02-03 17:37:19 +0000

    refactor index spec on asset

--- spec/views/content_assets/index.html.erb_spec.rb
+++ spec/views/content_assets/index.html.erb_spec.rb
@@ -2,15 +2,10 @@ require "rails_helper"
 
 RSpec.describe "content_assets/index", type: :view do
   before(:each) do
-    asset_file = ActiveStorage::Blob.create_after_upload!(
-      io: File.open("spec/fixtures/sample.jpeg", "rb"),
-      filename: "sample.jpeg",
-      content_type: "image/jpeg",
-    ).signed_id
-    @content_asset = ContentAsset.create!(title: "test title", avatar: asset_file, alt: "alt sample")
-    @content_asset2 = ContentAsset.create!(title: "test title 2", avatar: asset_file, alt: "alt sample 2")
-
-    @content_assets = [@content_asset, @content_asset2]
+    @content_assets = [
+      FactoryBot.create(:content_asset, title: "title 1", alt: "hello"),
+      FactoryBot.create(:content_asset, title: "title 2", alt: "hello 2 u"),
+    ]
   end
 
   it "renders a list of content_assets" do
@@ -20,5 +15,4 @@ RSpec.describe "content_assets/index", type: :view do
       rendered.include? asset.alt
     end
   end
-
 end
