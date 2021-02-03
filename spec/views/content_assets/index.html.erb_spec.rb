8a809a0a69c09e9efe14093195410f5d10dcdb3a
marc <marcrice12@gmail.com>
2021-02-03 16:03:04 +0000

    alt text displayed on index as a column, test covering multiple objects displayed on index

--- spec/views/content_assets/index.html.erb_spec.rb
+++ spec/views/content_assets/index.html.erb_spec.rb
@@ -2,7 +2,15 @@ require "rails_helper"
 
 RSpec.describe "content_assets/index", type: :view do
   before(:each) do
-    @content_assets = [FactoryBot.create(:content_asset), FactoryBot.create(:content_asset)]
+    asset_file = ActiveStorage::Blob.create_after_upload!(
+      io: File.open("spec/fixtures/sample.jpeg", "rb"),
+      filename: "sample.jpeg",
+      content_type: "image/jpeg",
+    ).signed_id
+    @content_asset = ContentAsset.create!(title: "test title", avatar: asset_file, alt: "alt sample")
+    @content_asset2 = ContentAsset.create!(title: "test title 2", avatar: asset_file, alt: "alt sample 2")
+
+    @content_assets = [@content_asset, @content_asset2]
   end
 
   it "renders a list of content_assets" do
@@ -12,4 +20,5 @@ RSpec.describe "content_assets/index", type: :view do
       rendered.include? asset.alt
     end
   end
+
 end
