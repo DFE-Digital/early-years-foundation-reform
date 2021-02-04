be40faae0b49b2619a231f3a548a26c51d029aac
guy-roberts <groberts2001@hotmail.com>
2021-02-04 16:35:41 +0000

    Layout for previewing pages ContentPage slug is now derived from the title Seed data for Counting

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -2,13 +2,20 @@ class ContentPage < ApplicationRecord
 
   acts_as_tree
   scope :top_level, -> { where("parent_id IS NULL") }
+  scope :order_by_position, -> { order('position ASC') }
+
   validates :title, format: { with: /\A[a-zA-Z0-9 ]+\Z/, message: "Title should only contain alphabet, numeric and space characters" }, presence: true, uniqueness: true
   validates :subtitle, presence: true
   validates :markdown, presence: true
   validates :seo, presence: true
+  # TODO Position needs to be unique for pages with the same parent
+  validates :position, presence: true, uniqueness: true, format: { with: /\A\d+\z/, message: "Integer only, no sign allowed" }
 
-  def slug
-    title.downcase.gsub(/ /, '-')
+  before_save :set_slug_from_title
+  before_create :set_slug_from_title
+
+  def set_slug_from_title
+    Rails.logger.debug "setting slug"
+    self.slug = title.downcase.gsub(/ /, '-')
   end
-
 end
