970ebcf50e2c81ceb6c086fb5cc6fab7278afd63
guy-roberts <groberts2001@hotmail.com>
2021-02-05 09:54:41 +0000

    The position of a ContentPage is unique among its siblings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_120501) do
+ActiveRecord::Schema.define(version: 2021_02_05_094052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_120501) do
     t.integer "parent_id"
     t.integer "position"
     t.string "published_status"
+    t.index ["position", "parent_id"], name: "index_content_pages_on_position_and_parent_id", unique: true
     t.index ["position"], name: "index_content_pages_on_position", unique: true
     t.index ["title"], name: "index_content_pages_on_title", unique: true
   end
