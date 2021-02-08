ca135299075c0ce5d2f1d7ca2e2e4ed6f61a7f25
guy-roberts <groberts2001@hotmail.com>
2021-02-08 15:27:14 +0000

    Removed unused migration

--- db/migrate/20210204120501_add_status_to_content_pages.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToContentPages < ActiveRecord::Migration[6.1]
-  change_table :content_pages, bulk: true do |t|
-    t.string :published_status
-  end
-end
--- spec/controllers/content_controller.rb
+++ spec/controllers/content_controller.rb
@@ -1,12 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ContentController, type: :controller do
-  # rubocop:disable Lint/AmbiguousBlockAssociation
-  # Disable this cop, since it's a deliberate idiom in RSpec.
-  # Note trying (:top_level_page) results in a different rubocop OCDism ie
-  # Lint/ParenthesesAsGroupedExpression: (...) interpreted as grouped expression.
-  let top_level_page { FactoryBot.create(:content_page, :top_level) }
-  # rubocop:enable Lint/AmbiguousBlockAssociation
+  let(:top_level_page) { FactoryBot.create(:content_page, :top_level) }
   describe "GET a top level page" do
     it "returns a top level page" do
       get "/content/" + top_level_page.slug
