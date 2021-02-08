36e9ca21076730e3324b116386aeddf169d1372e
guy-roberts <groberts2001@hotmail.com>
2021-02-08 21:32:30 +0000

    First try at the Hub/Landing page

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -6,6 +6,16 @@ class ContentController < ApplicationController
     @page = ContentPage.find_by_slug params["slug"]
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     @markdown = markdown.render(@page.markdown)
+    @top_level = ContentPage.top_level_nav
     @page
+  end
+
+  # GET /content
+  def index
+    @content_pages = ContentPage.top_level.order_by_position
+
+    respond_to do |format|
+      format.html { render layout: "landing_page_layout" }
+    end
   end
 end
