555877e789f0360e863484029a84b68661ffa254
guy-roberts <groberts2001@hotmail.com>
2021-02-09 14:23:51 +0000

    Simpler way to make menu links

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,11 +1,4 @@
 module ContentHelper
-
-  # TODO This needs reviewing
-  # BEWARE THIS IS monkey patching a route
-  def content_path(slug, childslug)
-    !childslug ? "/#{controller.controller_name}/#{slug}" : "/#{controller.controller_name}/#{slug}/#{childslug}"
-  end
-
   def subnav_link_css_classes(content_page)
     klasses = content_page.try(:parent_id) ? "govuk-link app-subnav__link govuk-link--no-visited-state" : "govuk-link app-subnav__link govuk-link--no-visited-state govuk-!-margin-bottom-2"
     content_page.slug == params[:slug] ? klasses + " app-subnav__section-item--current" : klasses
