2705c4241e032d016df6160a5731f5ba7ae288ff
Tony To <tonyto85@gmail.com>
2021-02-09 10:21:51 +0000

    /check endpoint for healthcheck pings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
+  def check
+    render json: { status: "OK", version: ENV["SHA"], environment: Rails.env }, status: :ok
+  end
+
 private
 
   def layout_by_resource
