f7208488c44330273fcc1a3c4dbfba118b67742d
marc <marcrice12@gmail.com>
2021-02-11 19:45:02 +0000

    govspeak replaces markdown

--- app/webpacker/custom/admin.js
+++ app/webpacker/custom/admin.js
@@ -2,12 +2,11 @@ import $ from 'jquery';
 
 export function createPreview(el, target) {
 
-  const showdown = require('showdown');
-  const previewContent = $(el).val();
-  const sanitizeContent = previewContent.replace(/<\/?[^>]+(>|$)/g, ""); //remove tags
-  const converter = new showdown.Converter();
-
-  $(target).html(converter.makeHtml(sanitizeContent))
+  $.post( "/govspeak", { input: $(el).val() })
+  .done(function( response ) {
+    $(target).html( response.html );
+  });
 
 }
+
 
