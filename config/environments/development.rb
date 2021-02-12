8d0269252f62070c69772ab7102149b345c42b01
Tony To <tonyto85@gmail.com>
2021-02-12 15:54:54 +0000

    Dev: adding host authorisation (#34)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,5 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "www.example.com"
+  config.hosts << "eyfs-dev.london.cloudapps.digital"
 end
