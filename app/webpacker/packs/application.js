88563582429f94cac9d5cef892c631fe613b4bac
marc <marcrice12@gmail.com>
2021-02-12 09:29:12 +0000

    webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,7 +2,6 @@ require.context('govuk-frontend/govuk/assets');
 
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
-import Turbolinks from 'turbolinks';
 import { initAll } from 'govuk-frontend';
 import $ from 'jquery';
 import {createPreview} from "custom/admin";
