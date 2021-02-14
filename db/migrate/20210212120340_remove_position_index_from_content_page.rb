24ee6d7edabb230289d2ec394942226def7639ac
guy-roberts <groberts2001@hotmail.com>
2021-02-14 20:16:39 +0000

    Rubocop wants migrations to be reversible

--- db/migrate/20210212120340_remove_position_index_from_content_page.rb
+++ db/migrate/20210212120340_remove_position_index_from_content_page.rb
@@ -1,5 +1,9 @@
 class RemovePositionIndexFromContentPage < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :completions, name: "index_content_pages_on_position"
+  def up
+    remove_index :content_pages, name: "index_content_pages_on_position"
+  end
+
+  def down
+    add_index :content_pages, :position, unique: true
   end
 end
