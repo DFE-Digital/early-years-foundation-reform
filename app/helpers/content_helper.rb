a4c81e474b597ae6482f6613a2faddeee9ecdf1a
marc <marcrice12@gmail.com>
2021-02-15 17:07:24 +0000

    mobile menu

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -2,4 +2,8 @@ module ContentHelper
   def path_for_this_page(page)
     "#{content_url}/#{page.slug}"
   end
+
+  def has_active_css(href)
+    "app-mobile-nav__subnav-item--current" if current_page?(href)
+  end
 end
--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -14,5 +14,19 @@ $(document).ready(function() {
   $('#markdown-editor').keyup(function() {
     createPreview('#markdown-editor','#markdown-render');
   });
+
+  //mobile nav
+  $( ".govuk-js-header-toggle" ).click(function() {
+    $( ".app-subnav--mobile" ).toggle();
+  });
+
+  $('.learning-section-mobile-nav > a').click(function() {
+    $(this).parent().children('ul.app-mobile-subnav-section').toggle();
+  }); 
+
+  $('.learning-section-mobile-nav .top-level-link').click(function(e) {
+    e.preventDefault();
+  });
   
+
 });
