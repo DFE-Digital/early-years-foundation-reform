a1e92f4425f566ec996f5c6a0e5d9ecf7cb1a59b
guy-roberts <groberts2001@hotmail.com>
2021-02-16 13:14:01 +0000

    WIP on seeds

--- db/seeds/communication_and_language_seeds.rb
+++ db/seeds/communication_and_language_seeds.rb
@@ -5,14 +5,14 @@
 markdown_for_communication_and_language = <<-MARKDOWN_FOR_COMMUNICATION
 ## Communication and language
 Access resources, activity ideas and advice for teaching communication and language to early years children.
-### Communication and language resources
+## Communication and language resources
 [Interactions]()
 
 [Exploring language]()
 
 [Listening and understanding]()
 
-### What the early years foundation stage (EYFS) framework says about communication and language
+## What the early years foundation stage (EYFS) framework says about communication and language
 The development of children's spoken language underpins all seven areas of learning and development.
 ### Read more
 Children's back-and-forth interactions from an early age form the foundations for language and cognitive development.
@@ -20,11 +20,35 @@ The number and quality of the conversations they have with adults and peers thro
 By commenting on what children are interested in or doing, and echoing back what they say with new vocabulary added, practitioners will build children's language effectively.
 Reading frequently to children, and engaging them actively in stories, non-fiction, rhymes and poems, and then providing them with extensive opportunities to use and embed new words in a range of contexts, will give children the opportunity to thrive.
 Through conversation, story-telling and role play, where children share their ideas with support and modelling from their teacher, and sensitive questioning that invites them to elaborate, children become comfortable using a rich range of vocabulary and language structures
+
+# A collapsible section with markdown
+<details>
+  <summary>Click to expand!</summary>
+
+  ## Heading
+  1. A numbered
+  2. list
+     * With some
+     * Sub bullets
+</details>
+
+$!
+This is a summary
+$!
+
+<div class="call-to-action">
+  <p>This is a call to action</p>
+</div>
+
+$AI
+This is additional information
+$AI
+
 MARKDOWN_FOR_COMMUNICATION
 
 communication_and_language = {
     title: "Communication and language",
-    subtitle: "Access resources, activity ideas and advice for teaching communication and language to early years children.",
+    subtitle: "Communication and language",
     markdown: markdown_for_communication_and_language,
     seo: "Early-Years-Foundation-Stage",
     position: 1,
@@ -38,31 +62,44 @@ communication_and_language_page.save!
 # rubocop:disable Layout/HeredocIndentation
 markdown_for_exploring_language = <<-MARKDOWN_FOR_EXPLORING_LANGUAGE
 Hundreds of hours of interactions enable a child to develop from babble, to words, to simple sentences.Studies show that once babies begin to understand words their vocabulary increases rapidly:
+
 - at age one, children recognise about 50 words
+
 - by age three, children recognise about 1,000 words
+
 - by age five, children recognise at least 10,000 words
+
 Having a large vocabulary helps children access more learning and allows them to make sense of the world around them. There are also lots of benefits from learning more than one language, including sign language.
 Research has shown lots that can be done in the early years to avoid inequalities in children's language development. Children from more disadvantaged backgrounds are likely to have fewer vocabulary experiences than those from more affluent backgrounds. This 'word gap' is narrowed by adults providing a language rich environment full of stories, rhymes, songs and play with words that tune into the interests of young learners.  Removing this inequality is a priority for practitioners. Children can make good progress with early language development with the right support.
 Children's language skills are strongly connected to a child's overall development and are a strong predictor of educational success. As speaking and listening develops children build foundations for literacy, for making sense of visual and verbal signs and ultimately for reading and writing.
 Communication and language is one of the EYFS prime areas which means that it is one of the important building blocks for all other areas. It is time sensitive and if not developed early is very difficult to acquire later.
+
 ## What this means in practice
 Strong language skills develop when children are part of playful, language rich environments with opportunities to learn new words. Hands-on experiences encourage learning and provide a context for new words to be explored. For example, learning vegetable names becomes easier when children are handling and/or tasting them.
 Songs and rhymes offer fun ways to explore the sounds of words and notice patterns to support language learning. Poems with actions and repeated parts help children tune in to the structure of spoken language and explore the feel of new words.
 Reading stories aloud and sharing books supports the development of language for thinking and understanding new concepts. Encouraging children to notice pictures, as well as understanding words, will strengthen language skills and widen vocabulary. Non-fiction and high quality texts encourage children to make sense of the world around them using language. Encouraging talk when sharing books is an excellent way to support communication and language.
 Pretend play and acting out stories extends language and practitioners offering props and ideas can deepen the learning. This may include imaginative play with small world resources, open ended materials such as blocks or loose parts. Creativity and problem solving through interesting experiences like woodwork, painting, exploring, observing nature and music are all ways to encourage language for thinking and expressing ideas. Adults can be skilled communication partners alongside children to support their language journey.
+
 ## Suggested activities
+
 ### Dialogic book talk
 Sharing a book with children is a fantastic way of exploring language together. Children will often want to talk about things they recognise, have already had experiences with and know about. Dialogic book talk is a way of encouraging children to make connections in their thinking when sharing and looking closely at the pages in a book. Allowing time to talk and think about what they notice in words and pictures, guided by an adult to extend vocabulary will help develop confident language skills.
+
 ### How does it work?
 Dialogic book talk can work as a small group activity or one to one with an adult. It is a shared activity during which adults and children talk about a book together. Open ended questions that encourage children to notice and think whilst reading the book is an effective prompt to support them to talk more about what is being read and what is in the pictures.
+
 ### Exploring language through dialogic book talk
 The adult listens carefully to the child and may repeat what the child says and expand their response eg. Child:"I can see a sheep" Adult: "Yes, I can see a fluffy sheep". This helps to encourage children's language confidence as well as introducing new vocabulary. Wondering questions can also extend children's thinking and encourage them to use their imagination and make predictions eg."I wonder what will happen to the farmer in the story?"
 ??Activity 2??
 ??[SME to complete]??
+
 ## Sensitively narrating children's play
 We know children learn language when it is introduced to them sensitively, for example:
+
 - at a time that is relevant to them
+
 - in manageable amounts
+
 - with plenty of time to process
 One way of encouraging children's understanding, and later use, of verbal language is to offer examples to them as they play. Successfully narrating children's play will be supported through the use of key universal strategies:
 **Expanding** – on what children say as you reply, adding one or two words. This shows the child you are interested and supports them to begin linking words and ideas in play. It can be helpful to remember that, if you ask the child one question and they are unable to reply, you may demonstrate responding by offering four comments. For example, 'what have you got?' (question) PAUSE 'It's a car', 'It's a red car', 'It's a red, fast car', 'It goes vrroomm!' (comments).
@@ -71,25 +108,37 @@ One way of encouraging children's understanding, and later use, of verbal langua
 It is important for practitioners to be flexible and responsive to different learning situations and interact in an authentic way having been invited into a child's play. This will help to ensure that thinking isn't interrupted and there is a sense of adult/child equality.
 ??(Video)??
 It shows a practitioner tuning in to a young child singing and joining in with the words- following his lead to acknowledge his interests and being a playful communication partner.
+
 ## What other nurseries and childminders are doing
+
 ### Supporting children with English as an Additional Language- Developing a shared context
 Central to our practice is establishing a shared context to start the conversational journey. Planning learning experiences through the 'lens of accessibility' with lots of ways for EAL children to attend, participate and communicate without needing words. Visual signs, simple language and familiar routines all help. Children develop the motivation of being valued members of the group and can share their knowledge and interests with us, even if we don't share their language.
 Familiar objects to model telling a story create a point of shared understanding and new vocabulary can be introduced. Natural, playful repetition found in all good story telling supports learners to thrive.
 **St Pauls Nursery School and Children's Centre**
+
 ## Summary
+
 -  Language development in the first years of life is critical to later educational success
+
 - Communication and language is a prime area in the EYFS and is time sensitive which means that it is difficult to acquire later on
+
 - Language development is best supported in a playful language environment full of stories, songs, rhymes, signs, talk and imaginative play
+
 - Early years practitioners are well positioned in every day practice to narrow the word gap and support all children to make good progress
+
 - Adults can be skilled communication partners alongside children to support their language journey.
+
 ## Next steps
+
 - Consider how a team understanding of the stages of typical communication and language development could further improve our practice?
+
 - Review how the environment (physical and emotional) meets all children's communication needs including non-verbal learners.
+
 - Strengthen partnerships with parents and professionals to support children's speech, language and communication
 MARKDOWN_FOR_EXPLORING_LANGUAGE
 exploring_language = {
     title: "Exploring language",
-    subtitle: "Access resources, activity ideas and advice for teaching communication and language to early years children.",
+    subtitle: "Exploring language",
     markdown: markdown_for_exploring_language,
     seo: "Early-Years-Foundation-Stage",
     position: 1,
@@ -216,7 +265,7 @@ Positive interactions that children have, both adult-child and child-child, enco
 MARKDOWN_FOR_INTERACTIONS
 interactions = {
     title: "Interactions",
-    subtitle: "Why interactions are important",
+    subtitle: "Interactions",
     markdown: markdown_for_interactions,
     seo: "Early-Years-Foundation-Stage",
     position: 2,
