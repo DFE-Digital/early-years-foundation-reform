d3395a3b1736451050fab8a159e7a5c4cac2ea5b
marcrice12 <78028419+marcrice12@users.noreply.github.com>
2021-02-16 10:20:23 +0000

    Govspeak (#32)
    * mac files
    
    * one line for connecting to docker container
    
    * markdown prev
    
    * layout for preview
    
    * todo: change back to 14
    
    * application layout
    
    * test webpacker loading js
    
    * feat: markdown preview
    
    * schema
    
    * increase textarea height
    
    * jquery
    
    * live preview
    
    * live preview
    
    * readme update
    
    * notify failed login attempts
    
    * schema
    
    * schema
    
    * live markdown preview
    
    * new gems
    
    * govspeak replaces markdown
    
    * govspeak replaces markdown as parser of choice
    
    * rename scss
    
    * content pages
    
    * govspeak controller
    
    * markdown output from db
    
    * remove extra lines in rspec test, for rubocop
    
    * webpacker
    
    * silence logging govspeak requests
    
    * govspeak controller spec removal
    
    * govspeak request spec
    
    * remove pub status
    
    * request spec govspeak
    
    * published state removed migration
    
    * gemfile
    
    Co-authored-by: Marc Rice <marcrice09@icloud.com>

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,9 +3,17 @@ require.context('../images', true)
 
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
-import Turbolinks from 'turbolinks';
 import { initAll } from 'govuk-frontend';
+import $ from 'jquery';
+import {createPreview} from "custom/admin";
 
 Rails.start();
-Turbolinks.start();
 initAll();
+
+$(document).ready(function() {
+
+  $('#markdown-editor').keyup(function() {
+    createPreview('#markdown-editor','#markdown-render');
+  });
+  
+});
