59fc2a7e72db9e01030f55462a4f4c0a98c4959a
borimogunje@and.digital <borimogunje@and.digital>
2021-02-19 11:19:59 +0000

    add js to allow clicking of div

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import doThis from "custom/click-7-areas"
+window.doThis = doThis
+
 console.log('Hello World from Webpacker')
