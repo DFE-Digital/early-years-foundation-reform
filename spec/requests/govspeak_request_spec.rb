e1fedc29344edfbc121a6597b774f64bd4526637
marc <marcrice12@gmail.com>
2021-02-25 15:40:35 +0000

    use faker for email

--- spec/requests/govspeak_request_spec.rb
+++ spec/requests/govspeak_request_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "Govspeak", type: :request do
 
     context "with a valid session" do
       it "renders a successful response" do
-        sign_in FactoryBot.create(:user, email: "govspeakguy@gmail.com")
+        sign_in FactoryBot.create(:user, email: Faker::Internet.email)
         post "/govspeak", params: valid_params
         expect(response.content_type).to eq "application/json; charset=utf-8"
         expect(JSON.parse(response.body)).to have_key("html")
@@ -25,7 +25,7 @@ RSpec.describe "Govspeak", type: :request do
       end
 
       it "returns valid html" do
-        sign_in FactoryBot.create(:user, email: "govspeakguy@gmail.com")
+        sign_in FactoryBot.create(:user, email: Faker::Internet.email)
         post "/govspeak", params: valid_params
         expect(JSON.parse(response.body)["html"]).to include('<h2 id="govspeak-is-magic">Govspeak is magic!</h2>')
       end
