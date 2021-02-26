9ea2c13da27f9356ac9a45d1e7d82e45d9e6128e
marc <marcrice12@gmail.com>
2021-02-26 15:41:22 +0000

    fix: merge of application.js

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -15,23 +15,6 @@ $(document).ready(function() {
   $('#markdown-editor').keyup(function() {
     createPreview('#markdown-editor','#markdown-render');
   });
-  
-  //----- mobile nav -----//
-  
-  //show mobile nav when clicking hamburger
-  $( ".govuk-js-header-toggle" ).click(function() {
-    $( ".app-subnav--mobile" ).toggleClass('app-mobile-nav--active');
-  });
-
-  //show subnav links when clicking top level
-  $('.learning-section-mobile-nav > a').click(function() {
-    $(this).parent().children('ul.app-mobile-subnav-section').toggle();
-  }); 
-
-  //disable top level section click on mobile nav
-  $('.learning-section-mobile-nav .top-level-link').click(function(e) {
-    e.preventDefault();
-  });
 
   //----- mobile nav -----//
 
