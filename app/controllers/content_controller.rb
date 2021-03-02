437018955d51caabb237025d4277ea6230939c3b
guy-roberts <groberts2001@hotmail.com>
2021-03-02 11:06:04 +0000

    WIP on hfeyp 200

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -5,7 +5,7 @@ class ContentController < ApplicationController
   def show
     @page = ContentPage.find_by_slug params["slug"]
 
-    doc = Govspeak::Document.new @page.markdown
+    doc = Govspeak::Document.new(@page.markdown, sanitize: false)
     @markdown = doc.to_html
     @page
   end
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module GovukRailsBoilerplate
     config.load_defaults 6.0
     config.autoloader = :classic
 
+    config.action_view.sanitized_allowed_tags = ['details', 'summary', 'a']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
