a50e638020044998cb0c13873f9d4baa21549b2b
Tony To <tonyto85@gmail.com>
2021-03-02 16:48:26 +0000

    Adding basic auth to content routes

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,6 +1,16 @@
 class ContentController < ApplicationController
   layout "content"
 
+  before_action :authenticate
+
+  def authenticate
+    return true unless Rails.env.production?
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV[:BASIC_AUTH_USER] && password == ENV[:BASIC_AUTH_PASSWORD]
+    end
+  end
+
   # GET /content/page_title
   def show
     @page = ContentPage.find_by_slug params["slug"]
