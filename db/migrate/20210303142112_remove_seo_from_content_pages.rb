0cab26916fb2c6260378826a0cf186eaa1a760df
guy-roberts <groberts2001@hotmail.com>
2021-03-03 18:12:56 +0000

    Remove seo and subtitle

---
+++ db/migrate/20210303142112_remove_seo_from_content_pages.rb
@@ -0,0 +1,15 @@
+class RemoveSeoFromContentPages < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :content_pages, bulk: true do |t|
+      t.string  :seo
+      t.string  :subtitle
+    end
+  end
+
+  def down
+    add_column :content_pages, bulk: true do |t|
+      t.string  :seo
+      t.string  :subtitle
+    end
+  end
+end
