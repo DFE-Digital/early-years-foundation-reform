376bd6f939da5dfb46deae2553b50f0fdc1b093f
Tony To <tonyto85@gmail.com>
2021-03-03 11:23:18 +0000

    Removed basic auth and adding robot meta to layout (#82)

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,16 +1,6 @@
 class ContentController < ApplicationController
   layout "content"
 
-  before_action :authenticate
-
-  def authenticate
-    return true unless Rails.env.production?
-
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
-
   # GET /content/page_title
   def show
     @page = ContentPage.find_by_slug params["slug"]
