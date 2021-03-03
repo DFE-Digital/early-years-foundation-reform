ea7a1f17c8c0bc61daae5ff649739a87ab53fad4
Tony To <tonyto85@gmail.com>
2021-03-03 10:24:20 +0000

    Fix basic auth env (#81)

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -7,7 +7,7 @@ class ContentController < ApplicationController
     return true unless Rails.env.production?
 
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV[:BASIC_AUTH_USER] && password == ENV[:BASIC_AUTH_PASSWORD]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
