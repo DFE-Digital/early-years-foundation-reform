d6585bb01f891a1b20bbf33ee2b26a783d82607b
Guy Roberts <groberts2001@hotmail.com>
2021-03-04 09:58:23 +0000

    Hfeyp 200 read more (#84)
    * Print button and Back to Top
    
    * WIP on hfeyp 200
    
    * Allow Summary details
    
    * About to merge main
    
    * Allowed tags in a constant
    
    * Ensure the same tags are allowed everytime Markdown is rendered
    
    * Rubocop
    
    * Rubocop
    
    * Redundant comment
    
    * rubocop
    
    * Not needed

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,11 +1,13 @@
 class ContentController < ApplicationController
   layout "content"
 
+  ALLOWED_TAGS = %w[details summary p h1 h2 h3 h4 ul li img div ol a span].freeze
+
   # GET /content/page_title
   def show
     @page = ContentPage.find_by_slug params["slug"]
 
-    doc = Govspeak::Document.new @page.markdown
+    doc = Govspeak::Document.new(@page.markdown, sanitize: true, allowed_elements: ContentController::ALLOWED_TAGS)
     @markdown = doc.to_html
     @page
   end
