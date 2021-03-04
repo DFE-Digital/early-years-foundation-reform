d7e4b5bc22f4f6e6cf2409f8b7bbda260bb15bd3
Tony To <tonyto85@gmail.com>
2021-03-04 17:25:49 +0000

    Adding cms app resource

--- terraform/app/modules/paas/main.tf
+++ terraform/app/modules/paas/main.tf
@@ -33,8 +33,39 @@ resource cloudfoundry_app web_app {
   environment = local.app_environment
 }
 
+resource cloudfoundry_app cms_app {
+  name = local.cms_app_name
+  command = var.cms_app_start_command
+  docker_image = var.app_docker_image
+  health_check_type = "http"
+  health_check_http_endpoint = "/check"
+  instances = var.web_app_instances
+  memory = var.web_app_memory
+
+  space = data.cloudfoundry_space.space.id
+  stopped = var.app_stopped
+  strategy = var.web_app_deployment_strategy
+  timeout = var.app_start_timeout
+  dynamic "service_binding" {
+    for_each = local.app_service_bindings
+    content {
+      service_instance = service_binding.value
+    }
+  }
+  routes {
+    route = cloudfoundry_route.cms_app_route.id
+  }
+  environment = local.app_environment
+}
+
 resource cloudfoundry_route web_app_route {
   domain   = data.cloudfoundry_domain.cloudapps_digital.id
   space    = data.cloudfoundry_space.space.id
   hostname = local.web_app_name
+}
+
+resource cloudfoundry_route cms_app_route {
+  domain   = data.cloudfoundry_domain.cloudapps_digital.id
+  space    = data.cloudfoundry_space.space.id
+  hostname = local.cms_app_name
 }
