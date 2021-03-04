4f74a55de00ed1c440bcf806c14ac92bd9b502df
guy-roberts <groberts2001@hotmail.com>
2021-03-04 09:22:04 +0000

    Not needed

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,6 @@ module GovukRailsBoilerplate
     config.load_defaults 6.0
     config.autoloader = :classic
 
-    config.action_view.sanitized_allowed_tags = ['details', 'summary', 'a']
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
