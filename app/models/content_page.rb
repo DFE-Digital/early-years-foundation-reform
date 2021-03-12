b6d9d00128db6e6b2480acbb6ae84c08c69bbee7
guy-roberts <groberts2001@hotmail.com>
2021-03-12 18:36:32 +0000

    Fix preview link in cms After editing a page, redirect to list of pages Rubocop changes

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -5,9 +5,9 @@ class ContentPage < ApplicationRecord
   scope :top_level, -> { where("parent_id IS NULL") }
   scope :order_by_position, -> { order("position ASC") }
 
-  CHARS_TO_OMIT_FROM_SLUG = ",.()"
+  CHARS_TO_OMIT_FROM_SLUG = ",.()".freeze
   ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,.\-\(\) ]+\Z/.freeze
-  TITLE_FORMAT_ERROR_MESSAGE = "should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}"
+  TITLE_FORMAT_ERROR_MESSAGE = "should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}".freeze
   validates :title, format: { with: ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES, message: TITLE_FORMAT_ERROR_MESSAGE }
   validates :title, presence: true, uniqueness: true
   validates :markdown, presence: true
@@ -21,7 +21,6 @@ class ContentPage < ApplicationRecord
     ContentPage.reorder
   end
 
-  # TODO: only works for two levels
   def full_path
     if parent
       "/" + parent.slug.to_s + "/" + slug.to_s
