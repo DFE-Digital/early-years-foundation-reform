6a00bbc2e5b1feb498e0d634de0b0820c1a7e100
guy-roberts <groberts2001@hotmail.com>
2021-03-16 11:17:23 +0000

    Migration for devise locakable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,7 +211,7 @@ Devise.setup do |config|
   config.maximum_attempts = 10
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 1.minute
+  config.unlock_in = 30.minutes
 
   # Warn on the last attempt before the account is locked.
   # config.last_attempt_warning = true
--- db/migrate/20210311123635_add_device_lockable.rb
+++ db/migrate/20210311123635_add_device_lockable.rb
@@ -1,3 +1,8 @@
+# Even the author of this Cop thinks it is too restrictive
+# https://github.com/rubocop/rubocop-rails/issues/110
+# It would make the migration harder to read and longer
+# rubocop:disable Rails/BulkChangeTable
+#
 class AddDeviceLockable < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :failed_attempts, :integer, default: 0
@@ -5,3 +10,5 @@ class AddDeviceLockable < ActiveRecord::Migration[6.1]
     add_column :users, :locked_at, :datetime
   end
 end
+
+# rubocop:enable Rails/BulkChangeTable
