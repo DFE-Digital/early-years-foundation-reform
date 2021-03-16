6c3be0a80f07fc1904d54342b2d76d5dc111000a
guy-roberts <groberts2001@hotmail.com>
2021-03-16 10:26:52 +0000

    Not testing devise now

--- spec/requests/devise_request_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "/content_pages", type: :request do
-
-  sign_in create(:user, name: 'John Doe')
-  visit root_path
-  expect(page).to include('John Doe')
-
-end
