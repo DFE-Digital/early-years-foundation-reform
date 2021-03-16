737d3d7697ef53004b594ff93f41592a5460a59c
guy-roberts <groberts2001@hotmail.com>
2021-03-16 11:58:49 +0000

    Use ContentPageController for preview

--- app/webpacker/custom/admin.js
+++ app/webpacker/custom/admin.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 export function createPreview(el, target) {
 
-  $.post( "/govspeak", { input: $(el).val() })
+  $.post( "/preview", { input: $(el).val() })
   .done(function( response ) {
     $(target).html( response.html );
   });
