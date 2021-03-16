a586a2a5b9ab50c3f6cf9eea23b370bdf37a9898
guy-roberts <groberts2001@hotmail.com>
2021-03-16 11:28:51 +0000

    Colons are allowed in title, not full stops

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -5,8 +5,8 @@ class ContentPage < ApplicationRecord
   scope :top_level, -> { where("parent_id IS NULL") }
   scope :order_by_position, -> { order("position ASC") }
 
-  CHARS_TO_OMIT_FROM_SLUG = ",.()".freeze
-  ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,.\-\(\) ]+\Z/.freeze
+  CHARS_TO_OMIT_FROM_SLUG = ",:()".freeze
+  ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,:\-\(\) ]+\Z/.freeze
   TITLE_FORMAT_ERROR_MESSAGE = "should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}".freeze
   validates :title, format: { with: ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES, message: TITLE_FORMAT_ERROR_MESSAGE }
   validates :title, presence: true, uniqueness: true
