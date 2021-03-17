c8716ca05773062ecab7a72fa0467c869d36bc3a
Guy Roberts <groberts2001@hotmail.com>
2021-03-17 12:44:28 +0000

    HFEYP 245 lock user account after failed attempts (#117)
    * WIP
    
    * WIP
    
    * Not testing devise now
    
    * Not testing devise now
    
    * Migration for devise lockable
    
    * Migration for devise locakable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_142112) do
+ActiveRecord::Schema.define(version: 2021_03_11_123635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,9 @@ ActiveRecord::Schema.define(version: 2021_03_03_142112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
