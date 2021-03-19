448dd3fc0e578f11e49403e7f1535f7b5f86387f
Brett McHargue <ebrett@users.noreply.github.com>
2021-03-19 15:02:26 +0000

    Add validations for content assets (#124)
    * Add validations for content assets
    
    * content type
    * extension
    
    * rubocop
    
    Co-authored-by: Guy Roberts <groberts2001@hotmail.com>

--- app/models/content_asset.rb
+++ app/models/content_asset.rb
@@ -1,6 +1,24 @@
 class ContentAsset < ApplicationRecord
+  VALID_FILE_EXTENSIONS = %w[.pdf .doc .docx .xls .xlsx .jpeg .jpg .png].freeze
+  VALID_CONTENT_TYPE = %w[
+    image/jpeg
+    application/pdf
+    application/msword
+    application/vnd.ms-excel
+    application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+    application/vnd.openxmlformats-officedocument.wordprocessingml.document
+  ].freeze
+
   has_one_attached :asset_file
   validates :title, presence: true
   validates :asset_file, presence: true
   validates :alt_text, presence: true
+  validate :asset_file_ext_validation
+  validates :asset_file, content_type: VALID_CONTENT_TYPE
+
+  def asset_file_ext_validation
+    return unless asset_file.attached?
+
+    errors.add(:asset_file, :invalid_extension) if VALID_FILE_EXTENSIONS.none? { |extension| asset_file.blob.filename.to_s.end_with?(extension) }
+  end
 end
