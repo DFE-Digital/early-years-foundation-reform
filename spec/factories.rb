e863a19c1651f50f3536abae62fe451c457b3e79
Guy Roberts <groberts2001@hotmail.com>
2021-04-16 18:15:03 +0100

    246 limit upload rate (#180)
    * Limit file uploads to one every 30 seconds
    (Penn testers insisted)
    
    * Test that ContentAssets can't be uploaded at the same time

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,11 +13,3 @@ FactoryBot.define do
     role { "reader" }
   end
 end
-
-FactoryBot.define do
-  factory :content_asset do
-    title { "Title" }
-    alt_text { "Sample alt text" }
-    asset_file { Rack::Test::UploadedFile.new("spec/fixtures/sample.jpeg", "image/jpeg") }
-  end
-end
