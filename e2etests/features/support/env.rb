852faf50d01b146762fb3758869cdf2156818865
guy-roberts <groberts2001@hotmail.com>
2021-05-06 10:11:49 +0100

    Forgot this in the merge

--- e2etests/features/support/env.rb
+++ e2etests/features/support/env.rb
@@ -5,7 +5,7 @@ require "selenium-webdriver"
 require "axe/cucumber/step_definitions"
 require "webdrivers/chromedriver"
 
-TARGET_URL = "http://help-for-early-years-providers.education.gov.uk/".freeze
+TARGET_URL = "http://help-for-early-years-providers.education.gov.uk/"
 
 if ENV["headless"]
   Capybara.default_driver = :poltergeist
@@ -19,9 +19,9 @@ end
 
 ATTEMPTS = 5
 
-HELP_FOR_EARLY_YEARS_PROVIDERS = "#main-content > div.landing-page__section--grey > div > ul".freeze
-LI_VALUES = "li".freeze
-LEFT_PANE_MENU = "body > main > div > div > nav > nav".freeze
-LP_LEARNING_AREAS = "h4".freeze
-PREVIOUS_PAGE = "li.gem-c-pagination__item.gem-c-pagination__item--previous > a > span.gem-c-pagination__link-label".freeze
-NEXT_PAGE = "li.gem-c-pagination__item.gem-c-pagination__item--next > a > span.gem-c-pagination__link-label".freeze
+HELP_FOR_EARLY_YEARS_PROVIDERS = "#main-content > div.landing-page__section--grey > div > ul"
+LI_VALUES = "li"
+LEFT_PANE_MENU = "body > main > div > div > nav > nav"
+LP_LEARNING_AREAS = "h4"
+PREVIOUS_PAGE = "li.gem-c-pagination__item.gem-c-pagination__item--previous > a > span.gem-c-pagination__link-label"
+NEXT_PAGE = "li.gem-c-pagination__item.gem-c-pagination__item--next > a > span.gem-c-pagination__link-label"
