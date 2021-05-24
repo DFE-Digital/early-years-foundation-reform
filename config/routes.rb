b305371537c2c4972d76703291f5375379b20cbf
Ife Akinbolaji <i.akinbolaji@gmail.com>
2021-05-24 16:48:35 +0100

    Add routes for service down page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
+  get "/503", to: "errors#service_down", via: :all
 
   resources :settings, only: %i[show create]
 
