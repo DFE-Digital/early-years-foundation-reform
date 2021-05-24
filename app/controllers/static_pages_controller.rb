b1f46b47cdbe47352eec9daf648f99962c78b4dd
Brett McHargue <brett@therubytroubadour.com>
2021-05-24 13:44:55 +0100

    Use content layout for application
    * replace application.html.erb with content.html.erb
    * add flags to show/hide parts of the layout (navigation, breadcrumbs,
      helpful tools)
    * add page title to static pages
    * remove content layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  before_action do |controller|
+    @page = OpenStruct.new(title: t(params[:action], default: params[:action].humanize))
+  end
+
   def accessibility_statement; end
 
   def disclaimer; end
