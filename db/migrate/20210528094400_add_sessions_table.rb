317b58c640168b9c5854cb2df5c915f88ea5c06b
Brett McHargue <brett@therubytroubadour.com>
2021-05-28 12:40:54 +0100

    Fix rubocop errors and update changelog

--- db/migrate/20210528094400_add_sessions_table.rb
+++ db/migrate/20210528094400_add_sessions_table.rb
@@ -1,20 +1,12 @@
 class AddSessionsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
-<<<<<<< HEAD
       t.string :session_id, null: false
-=======
-      t.string :session_id, :null => false
->>>>>>> 4764d047 (Add active record session store)
       t.text :data
       t.timestamps
     end
 
-<<<<<<< HEAD
     add_index :sessions, :session_id, unique: true
-=======
-    add_index :sessions, :session_id, :unique => true
->>>>>>> 4764d047 (Add active record session store)
     add_index :sessions, :updated_at
   end
 end
