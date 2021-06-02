8fba2f8cd0f0572a8c9f876af2f5ee947a400170
Brett McHargue <brett@therubytroubadour.com>
2021-06-02 16:11:28 +0100

    Update bundle to run rubocop

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -6,7 +6,7 @@ class ContentPage < ApplicationRecord
   scope :order_by_position, -> { order("position ASC") }
 
   CHARS_TO_OMIT_FROM_SLUG = ",:()".freeze
-  ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,:\-\(\) ]+\Z/.freeze
+  ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,:\-() ]+\Z/.freeze
   TITLE_FORMAT_ERROR_MESSAGE = "should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}".freeze
   validates :title, format: { with: ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES, message: TITLE_FORMAT_ERROR_MESSAGE }
   validates :title, presence: true, uniqueness: true
@@ -29,7 +29,7 @@ class ContentPage < ApplicationRecord
 
   def full_path
     if parent
-      "/" + parent.slug.to_s + "/" + slug.to_s
+      "/#{parent.slug}/#{slug}"
     else
       slug.to_s
     end
