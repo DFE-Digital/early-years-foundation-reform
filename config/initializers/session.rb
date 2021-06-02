24d1d75c99d4309d9b02d7b726e75771e9d4e71e
Brett McHargue <ebrett@users.noreply.github.com>
2021-06-02 17:14:13 +0100

    239 Sessions Table - drop table (#273)
    * Add active record session store
    
    * Fix rubocop errors and update changelog
    
    * Upgrade bundler gem in dockerfiles
    
    * Drop sessions table
    
    * bad idea, as the public site does not have write access to db
    
    * Update bundle to run rubocop

--- config/initializers/session.rb
+++ config/initializers/session.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :active_record_store, key: "_help_for_early_years_providers_session"
+Rails.application.config.session_store :cookie_store, key: "_help_for_early_years_providers_session"
