fbf882bd3b25b222129246d2e43f849b8b46d465
Brett McHargue <brett@therubytroubadour.com>
2021-06-15 16:33:00 +0100

    Update passwords in seed

--- db/seeds/create_users.rb
+++ db/seeds/create_users.rb
@@ -1,13 +1,13 @@
 unless User.exists?(email: "dev@hfeyp.com")
   User.create! do |u|
     u.email = "dev@hfeyp.com"
-    u.password = "Partyw0rd"
+    u.password = "11abXY!@"
     u.role = "admin"
   end
 
   User.create! do |u|
     u.email = "guy@home.com"
-    u.password = "guy0guy2021"
+    u.password = "11abXY!@"
     u.role = "editor"
   end
 end
