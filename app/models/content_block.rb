8f707325dd5f6e0b7396d7e2a17198ffb103d5a4
borimogunje@and.digital <borimogunje@and.digital>
2021-06-17 16:27:21 +0100

    create content block model

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,5 +1,7 @@
 class ContentBlock < ApplicationRecord
+    audited
 
     validates :markdown, presence: true
-
+    validates :name, presence: true
+    validates :description, presence: true
 end
