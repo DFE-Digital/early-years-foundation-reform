5442382ba1dab60e54c7809d318301875cf9ec56
Brett McHargue <brett@therubytroubadour.com>
2021-06-21 12:23:41 +0100

    Updates for rubocop

--- spec/requests/admin/user_request_spec.rb
+++ spec/requests/admin/user_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "/admin/users", type: :request do
-  context 'as admin' do
+  context "as admin" do
     describe "GET /index" do
       it "renders a successful response" do
         sign_in create(:admin)
@@ -14,7 +14,7 @@ RSpec.describe "/admin/users", type: :request do
     end
   end
 
-  context 'not as admin' do
+  context "not as admin" do
     describe "GET /index as editor" do
       it "renders a unsuccessful response" do
         sign_in create(:editor)
@@ -25,7 +25,7 @@ RSpec.describe "/admin/users", type: :request do
         expect(response).to_not be_successful
       end
     end
-    
+
     describe "GET /index not signed in" do
       it "renders a unsuccessful response" do
         create_list(:editor, 10)
