15d966810b6d950cc539f73874037ce47a159913
Brett McHargue <ebrett@users.noreply.github.com>
2021-06-24 10:40:58 +0100

    481 Combine error messages for email (#312)
    * Fix issues
    
    * Updates for rubocop
    
    * CHANGELOG
    
    * Do password tooltip
    
    * an issue with the field width, putting the tooltip around the password
      fields causes the width to change (get smaller)
    
    * Combine email address error validation
    
    * Order of validations appears to be important
    
    * make sure email validation happens before password validation
    * use email address for label
    
    * rubocop fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  specify { expect(FactoryBot.create(:user)).to be_valid }
+  specify { expect(create(:user)).to be_valid }
+
+  context "validation" do
+    specify { expect { create(:editor, email: "") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "@") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "1@2") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "!£$%") }.to raise_error(ActiveRecord::RecordInvalid) }
+  end
 end
