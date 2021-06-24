8b08178007283cae279e7a081bc065d2c48035ea
Brett McHargue <brett@therubytroubadour.com>
2021-06-24 09:39:22 +0100

    rubocop fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, type: :model do
   specify { expect(create(:user)).to be_valid }
 
   context "validation" do
-    specify { expect{create(:editor, email: "")}.to raise_error(ActiveRecord::RecordInvalid) }
-    specify { expect{create(:editor, email: "@")}.to raise_error(ActiveRecord::RecordInvalid) }
-    specify { expect{create(:editor, email: "1@2")}.to raise_error(ActiveRecord::RecordInvalid) }
-    specify { expect{create(:editor, email: "!£$%")}.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "@") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "1@2") }.to raise_error(ActiveRecord::RecordInvalid) }
+    specify { expect { create(:editor, email: "!£$%") }.to raise_error(ActiveRecord::RecordInvalid) }
   end
 end
