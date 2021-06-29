3f2c3fb18805dc9954a5f352e75291a3a8bc2c78
Brett McHargue <ebrett@users.noreply.github.com>
2021-06-29 13:11:38 +0100

    Password update fixed (#316)
    * Password update fixed
    
    * Styling on tooltip hint for password
    
    * Fix password update
    
    Password update was not erring correctly
    
    * Make tooltip style consistent
    
    * Rubocop cleanup
    
    * Fix save
    
    * Use standard messages for notifications
    
    * Need to update the spec too!

--- spec/features/admin/user_admin_page_spec.rb
+++ spec/features/admin/user_admin_page_spec.rb
@@ -15,16 +15,17 @@ RSpec.feature "User administration", type: :feature do
   end
 
   context "with session" do
+    let(:barbara) { User.find_by(email: "birdsofprey@education.gov.uk") }
     scenario "visits admin root" do
-      login_as(FactoryBot.create(:editor))
+      login_as(barbara)
       visit "/admin"
       expect(current_path).to eq(admin_root_path)
       active_nav = page.find_by_id("navigation").first("li.govuk-header__navigation-item--active")
       title = page.find("h1")
       expect(active_nav).to have_css("a[href='/admin/users']", visible: true, text: "Admin")
-      expect(title).to have_text("User Administration")
+      expect(title).to have_text("User administration")
 
-      expect(page).to have_link(href: "/admin/users/new", visible: true, text: "Add User")
+      expect(page).to have_link(href: "/admin/users/new", visible: true, text: "Add user")
 
       expect(page).to have_css("th", text: "Name")
       expect(page).to have_css("th", text: "Email")
@@ -36,19 +37,19 @@ RSpec.feature "User administration", type: :feature do
     end
 
     scenario "a user with admin role should not be able to assign herself to another role" do
-      user_who_logs_in = FactoryBot.create(:admin)
-      login_as(user_who_logs_in)
-      visit edit_admin_user_path(user_who_logs_in)
+      login_as(barbara)
+      visit edit_admin_user_path(barbara)
 
       expect(page).to have_text("Your role is admin and you cannot change it")
     end
 
     scenario "the list of users should be ordered by email address, ascending" do
-      user_who_logs_in = FactoryBot.create(:admin)
-      login_as(user_who_logs_in)
+      login_as(barbara)
 
       visit admin_users_path
-      expect(page.body).to have_text(/.*birdsofprey.*jjj.*pwhite.*/)
+      expect_within_table_row(1, ["Barbara Gordon", "birdsofprey@education.gov.uk", "Administrator"])
+      expect_within_table_row(2, ["J Jonah Jameson", "jjj@education.gov.uk", "Editor"])
+      expect_within_table_row(3, ["Perry White", "pwhite@education.gov.uk", "Editor"])
     end
   end
 end
