4d903486c10cee1dc997ca17906247dcec3a32c3
guy-roberts <groberts2001@hotmail.com>
2021-07-12 10:02:26 +0100

    Rubocop

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,8 +1,6 @@
 class ContentBlock < ApplicationRecord
   audited
-  # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :name, uniqueness: true
-  # rubocop:enable Rails/UniqueValidationWithoutIndex
   validates :name, presence: true, format: { without: /\s/, message: "must contain no spaces" }
   validates :description, presence: true
 
