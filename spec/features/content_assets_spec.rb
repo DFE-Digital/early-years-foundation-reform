76643fb6e35b493881509954f22a39a9e83fc46e
Brett McHargue <brett@therubytroubadour.com>
2021-07-15 13:37:04 +0100

    Rubocop!

--- spec/features/content_assets_spec.rb
+++ spec/features/content_assets_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "New content assets page", type: :feature do
   scenario "should not have any accessibility errors" do
     sign_in create :admin
     visit "/cms/assets/new"
-  
+
     expect(page).to be_axe_clean
   end
 end
