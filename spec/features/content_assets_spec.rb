929cc4bd18189df66556eb5e682b1c7f71e3b484
Brett McHargue <brett@therubytroubadour.com>
2021-07-15 13:32:34 +0100

    Add axe clean spec for content assets new page

---
+++ spec/features/content_assets_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature "New content assets page", type: :feature do
+  scenario "should not have any accessibility errors" do
+    sign_in create :admin
+    visit "/cms/assets/new"
+  
+    expect(page).to be_axe_clean
+  end
+end
