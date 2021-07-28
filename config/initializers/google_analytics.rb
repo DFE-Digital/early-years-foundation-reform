5ff81f18899712e56cb229fee6e727defaf41954
Brett McHargue <brett@therubytroubadour.com>
2021-07-28 14:00:00 +0100

    Correct the credentials for GTM

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module GoogleAnalytics
-  TRACKING_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space, :tracking_id
+  TRACKING_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space.to_sym, :tracking_id
   JS_URL = "https://www.googletagmanager.com/ns.html?id=#{GoogleAnalytics::TRACKING_ID}"
 end
