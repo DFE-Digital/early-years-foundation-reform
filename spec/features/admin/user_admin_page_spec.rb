6c9dc497057b82547ed7d6b8a15b8ffd3d5d1b81
guy-roberts <groberts2001@hotmail.com>
2021-08-20 15:56:52 +0100

    User's first name and last name are mandatory.  Typo in title for editing pages

--- spec/features/admin/user_admin_page_spec.rb
+++ spec/features/admin/user_admin_page_spec.rb
@@ -51,5 +51,35 @@ RSpec.feature "User administration", type: :feature do
       expect_within_table_row(2, ["J Jonah Jameson", "jjj@education.gov.uk", "Editor"])
       expect_within_table_row(3, ["Perry White", "pwhite@education.gov.uk", "Editor"])
     end
+
+    scenario "An admin user should not be able to create a new user without providing a first name" do
+      login_as(barbara)
+      visit new_admin_user_path(barbara)
+
+      fill_in "user-last-name-field", with: Faker::Name.last_name
+      fill_in "user-password-field", with: "WordyWord2021%%"
+      fill_in "user-password-confirmation-field", with: "WordyWord2021%%"
+      choose("user-role-editor-field", visible: false)
+      fill_in "user-email-field", with: "buzzard@education.gov.uk"
+
+      click_button "Save"
+
+      expect(page).to have_text("First name must not be blank")
+    end
+
+    scenario "An admin user should not be able to create a new user without providing a last name" do
+      login_as(barbara)
+      visit new_admin_user_path(barbara)
+
+      fill_in "user-first-name-field", with: Faker::Name.first_name
+      fill_in "user-password-field", with: "WordyWord2021%%"
+      fill_in "user-password-confirmation-field", with: "WordyWord2021%%"
+      choose("user-role-editor-field", visible: false)
+      fill_in "user-email-field", with: "moggy@education.gov.uk"
+
+      click_button "Save"
+
+      expect(page).to have_text("Last name must not be blank")
+    end
   end
 end
