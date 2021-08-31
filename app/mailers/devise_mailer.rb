2cd6e000c3c2e8932702aac6a0886244d56bdc67
Brett McHargue <ebrett@users.noreply.github.com>
2021-08-31 11:06:44 +0100

    Revert "503 notifications (#390)" (#392)
    This reverts commit 1a3a0ab075967c53218641f4ba4ef182da20f38f.

--- app/mailers/devise_mailer.rb
+++
@@ -1,8 +0,0 @@
-class DeviseMailer < Devise::Mailer
-  STANDARD_EMAIL_TEMPLATE = "e2c26677-797d-481c-abaf-8d3fafa315b0".freeze
-
-  def devise_mail(record, action, opts = {})
-    initialize_from_record(record)
-    Mail::Notify::Mailer.view_mail(STANDARD_EMAIL_TEMPLATE, headers_for(action, opts))
-  end
-end
