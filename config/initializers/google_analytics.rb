cae1213220544f0314f2412581a5b9605a0fac4c
Brett McHargue <brett@therubytroubadour.com>
2021-09-06 12:43:04 +0100

    Add GA environment variables

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module GoogleAnalytics
-  CONTAINER_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space.to_sym, :container_id
-  TRACKING_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space.to_sym, :tracking_id
+  #CONTAINER_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space.to_sym, :container_id
+  #TRACKING_ID = Rails.application.credentials.dig :google_analytics, Rails.configuration.space.to_sym, :tracking_id
+  CONTAINER_ID = ENV['GOOGLE_ANALYTICS_CONTAINER_ID']
+  TRACKING_ID = ENV['GOOGLE_ANALYTICS_TRACKING_ID']
   JS_URL = "https://www.googletagmanager.com/ns.html?id=#{GoogleAnalytics::TRACKING_ID}"
 end
