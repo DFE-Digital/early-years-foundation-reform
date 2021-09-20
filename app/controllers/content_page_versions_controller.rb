db783d1defc5478d4111ec5ed5f1cfe5707cf3d0
guy-roberts <groberts2001@hotmail.com>
2021-09-20 13:21:12 +0100

    Bello's review tickets (569 - 572)

--- app/controllers/content_page_versions_controller.rb
+++ app/controllers/content_page_versions_controller.rb
@@ -6,14 +6,18 @@ class ContentPageVersionsController < ApplicationController
 
   def destroy
     authorize @content_page_version, :destroy?
+    authorize @content_page_version.content_page, :destroy?
     @parent_page = @content_page_version.content_page
 
-    if @parent_page.content_page_versions.count == 1
-      redirect_to "#{content_page_path(@parent_page)}/versions", notice: "Cannot delete the last remaining version"
-    else
-      @content_page_version.destroy!
-      redirect_to "#{content_page_path(@parent_page)}/versions", notice: "Content page version was successfully destroyed."
+    @content_page_version.destroy!
+
+    # Odd requirement. This is a round about way to delete a page, by un-publishing it
+    # and deleting all its versions. Normally pages can not be deleted, just un-published
+    if @parent_page.content_page_versions.count.zero?
+      @parent_page.destroy!
     end
+
+    redirect_to content_pages_path, notice: "Content page version was successfully destroyed."
   end
 
   # GET /content_page_version/1/edit
@@ -24,6 +28,7 @@ class ContentPageVersionsController < ApplicationController
 
   def update
     authorize @content_page_version, :update?
+    @content_page_version.author = current_user.name
 
     if @content_page_version.update(content_page_version_params)
       redirect_to versions_content_page_url(@content_page_version.content_page), notice: "This version was successfully updated"
@@ -48,11 +53,6 @@ class ContentPageVersionsController < ApplicationController
   end
 
   def publish
-    # "content_page_id"=>"1", "id"=>"19"
-    # If the page is already published, copy it to a new draft
-    #
-    # Copy the markdown and title into the ContentPage
-    # Change the author
     @page = @content_page_version.content_page
     if @page.is_published
       ContentPageVersion.create!(title: @page.title,
@@ -60,11 +60,14 @@ class ContentPageVersionsController < ApplicationController
                                  content_page_id: @page.id,
                                  author: current_user.name)
       # Copy the markdown and title into the ContentPage
-      @page.update!(markdown: @content_page_version.markdown)
+      @page.update!(markdown: @content_page_version.markdown,
+                    author: current_user.name)
     else
-      @page.update!(markdown: @content_page_version.markdown, is_published: true)
+      @page.update!(markdown: @content_page_version.markdown, is_published: true, author: current_user.name)
+      # Delete the version that this page was published from
+      @content_page_version.destroy!
     end
-    redirect_to versions_content_page_path(@page), notice: "Published !"
+    redirect_to versions_content_page_path(@page), notice: "Published"
   end
 
 private
