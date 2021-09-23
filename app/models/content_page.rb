f52d1fe14cd7d121c6dd6a21c9b7720154e53a14
guy-roberts <groberts2001@hotmail.com>
2021-09-23 10:49:49 +0100

    Error messages for ContentPage editing

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -10,7 +10,7 @@ class ContentPage < ApplicationRecord
 
   CHARS_TO_OMIT_FROM_SLUG = ",:()".freeze
   ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES = /\A[a-zA-Z0-9,:\-() ]+\Z/.freeze
-  TITLE_FORMAT_ERROR_MESSAGE = "should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}".freeze
+  TITLE_FORMAT_ERROR_MESSAGE = "Heading should only contain alphabetic, numeric and -#{CHARS_TO_OMIT_FROM_SLUG}".freeze
   validates :title, format: { with: ONLY_ALPHA_NUMERIC_COMMA_HYPHEN_SPACE_AND_ROUND_BRACES, message: TITLE_FORMAT_ERROR_MESSAGE }
   validates :title, presence: true, uniqueness: true
   validates :markdown, presence: true
