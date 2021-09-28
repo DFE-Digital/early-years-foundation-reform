6288bb6f2202481cf021a2ec853b2ddc27ccaf09
Brett McHargue <brett@therubytroubadour.com>
2021-09-28 15:58:23 +0100

    Update errors, correct dom id for article index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,12 +13,10 @@ class Article < ApplicationRecord
   has_one_attached :featured_image
   has_one_attached :thumbnail_image
 
-  validates :title, :description, :markdown, :featured_image, :thumbnail_image, presence: true
+  validates :title, :description, :markdown, :featured_image, :thumbnail_image, :featured_alt_text, :thumbnail_alt_text, presence: true
 
   validate :image_file_ext_validation
-  validates :featured_image, content_type: VALID_CONTENT_TYPE
-  validates :thumbnail_image, content_type: VALID_CONTENT_TYPE
-  validates :featured_image, :thumbnail_image, antivirus: true
+  validates :featured_image, :thumbnail_image, content_type: VALID_CONTENT_TYPE, antivirus: true
 
   def image_file_ext_validation
     if featured_image.attached? && VALID_FILE_EXTENSIONS.none? { |extension| featured_image.blob.filename.to_s.downcase.end_with?(extension) }
