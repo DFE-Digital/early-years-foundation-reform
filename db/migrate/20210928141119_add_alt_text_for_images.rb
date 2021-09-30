31943f2d3068c4f9d6b995b8ebaf903daaa823db
Brett McHargue <brett@therubytroubadour.com>
2021-09-30 19:57:22 +0100

    Missed colon

--- db/migrate/20210928141119_add_alt_text_for_images.rb
+++ db/migrate/20210928141119_add_alt_text_for_images.rb
@@ -3,7 +3,7 @@ class AddAltTextForImages < ActiveRecord::Migration[6.1]
     change_table :articles, bulk: true do
       add_column :featured_alt_text, :string
       add_column :thumbnail_alt_text, :string
-      add_column :author, string
+      add_column :author, :string
       add_column :published_at, :datetime
     end
   end
