5365282c6c1e100c007271825764037061880678
borimogunje@and.digital <borimogunje@and.digital>
2021-10-15 10:40:38 +0100

    add hash for featured pages

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -9,7 +9,12 @@ class ContentController < ApplicationController
 
   # This is a page whose title and children's titles are rendered
   # a block in the landing_page_layout template
-  FEATURED_PAGE_TITLES = ["Get help to improve your practice", "Safeguarding and welfare"].freeze
+  FEATURED_PAGE_TITLES = ["Get support and guidance",
+    "Get help to improve your practice", 
+    "Safeguarding and welfare"].freeze
+  FEATURED_PAGE_HASH = { "Get support and guidance" => "Get support", \
+    "Get help to improve your practice" => "Get help", \
+    "Safeguarding and welfare" => "Safeguarding" }.freeze
 
   # GET /page_title
   def show
