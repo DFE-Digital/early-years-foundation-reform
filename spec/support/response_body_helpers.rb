903b7fa573a7ba7b6498c47e154d16ad16fda658
Rob Nichols <reggieb@users.noreply.github.com>
2021-12-02 15:26:49 +0000

    Hide support articles section unless there are published articles (#501)

--- spec/support/response_body_helpers.rb
+++ spec/support/response_body_helpers.rb
@@ -4,7 +4,12 @@ module ResponseBodyHelpers
   # Removes HTML escaping from response.body so for example `don&#39;t` in the page body is returned as `don't`
   # which is then easier to compare within a spec.
   def unescaped_response_body
-    CGI.unescapeHTML(response.body)
+    @unescaped_response_body ||= CGI.unescapeHTML(response.body)
+  end
+
+  # Returns the HTML in the response in a format (Nokogiri HTML document) that can be navigated via css and xpath.
+  def html_document
+    @html_document ||= Nokogiri::HTML(response.body)
   end
 end
 
