2302f7ee8a4330a34c00fefc1cafc64fe6b02418
borimogunje@and.digital <borimogunje@and.digital>
2021-12-03 01:35:09 +0000

    WIP: refactor content assets into admin namespace

--- app/controllers/admin/content_assets_controller.rb
+++ app/controllers/admin/content_assets_controller.rb
@@ -1,8 +1,6 @@
-class ContentAssetsController < ApplicationController
-  layout "cms"
-
-  before_action :authenticate_user!
-  before_action :set_content_asset, only: %i[show edit update destroy]
+module Admin
+  class ContentAssetsController < AdminController
+    before_action :set_content_asset, only: %i[show edit update destroy]
 
   def index
     @content_assets = ContentAsset.all
@@ -22,7 +20,7 @@ class ContentAssetsController < ApplicationController
     @content_asset = ContentAsset.new(content_asset_params)
 
     authorize @content_asset, :create?
-
+    # Not sure about what to do here
     if upload_rate_limit_exceeded?
       redirect_to content_assets_url, notice: "Only one upload is allowed in each 30 seconds, please wait"
     elsif @content_asset.save
@@ -35,33 +33,34 @@ class ContentAssetsController < ApplicationController
   def update
     authorize @content_asset, :update?
 
-    if @content_asset.update(content_asset_params)
-      redirect_to @content_asset, notice: "Content asset was successfully updated."
-    else
-      render :edit
+      if @content_asset.update(content_asset_params)
+        redirect_to @content_asset, notice: "Content asset was successfully updated."
+      else
+        render :edit
+      end
     end
-  end
 
   def destroy
     authorize @content_asset, :destroy?
 
-    @content_asset.destroy!
-    redirect_to content_assets_url, notice: "Content asset was successfully destroyed."
-  end
+      @content_asset.destroy!
+      redirect_to content_assets_url, notice: "Content asset was successfully destroyed."
+    end
 
-private
+  private
 
-  def upload_rate_limit_exceeded?
-    ContentAsset.where(updated_at: 30.seconds.ago..Time.zone.now).count.positive?
-  end
+    def upload_rate_limit_exceeded?
+      ContentAsset.where(updated_at: 30.seconds.ago..Time.zone.now).count.positive?
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_content_asset
-    @content_asset = ContentAsset.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_content_asset
+      @content_asset = ContentAsset.find(params[:id])
+    end
 
-  # Only allow a list of trusted parameters through.
-  def content_asset_params
-    params.require(:content_asset).permit(:title, :asset_file, :alt_text)
+    # Only allow a list of trusted parameters through.
+    def content_asset_params
+      params.require(:content_asset).permit(:title, :asset_file, :alt_text)
+    end
   end
 end
