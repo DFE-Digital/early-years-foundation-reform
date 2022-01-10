3c3f884b8a3f50e011f3422f4f8034561db5fdb8
Brett McHargue <brett@therubytroubadour.com>
2022-01-10 11:14:56 +0000

    Add guard to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,10 @@ module ApplicationHelper
   end
 
   def feature_image(page:)
-    if page.featured_image.attached?
-      tag.div(class: "page-img-container") do
-        image_tag(url_for(page.featured_image), class: "page-responsive-img", alt: page.featured_alt_text, title: page.title, width: "500px")
-      end
+    return unless page.featured_image.attached?
+
+    tag.div(class: "page-img-container") do
+      image_tag(url_for(page.featured_image), class: "page-responsive-img", alt: page.featured_alt_text, title: page.title, width: "500px")
     end
   end
 end
