ed437fdb46fc076db8dffcf44878a224f9d9daab
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2022-01-17 12:50:58 +0000

    HFEYP-652 remove duplicate spec (#554)
    * remove duplicate and flaky spec
    
    * remove empty spec

--- spec/features/drafting_and_publishing_spec.rb
+++ spec/features/drafting_and_publishing_spec.rb
@@ -21,35 +21,5 @@ RSpec.feature "Drafting and publishing pages", type: :feature do
       expect(saved_page.is_published).to eq(false)
       expect(saved_page.content_page_versions.count).to eq(1)
     end
-
-    it "should delete an unpublished page when the last draft is deleted" do
-      sign_in FactoryBot.create(:user, :editor)
-      attributes = FactoryBot.attributes_for :content_page
-      parent_page = FactoryBot.create(:content_page, :top_level)
-
-      visit "/admin/pages/new?parent_id=#{parent_page.id}"
-
-      page.find_field("content_page[title]").set(attributes[:title])
-      page.find_field("content_page[markdown]").set(attributes[:markdown])
-      page.find_field("content_page[position]").set(rand(10_000))
-
-      page.click_button("Save")
-      saved_page = ContentPage.find_by_title attributes[:title]
-
-      visit versions_admin_content_page_path(saved_page)
-      page.click_link("Delete")
-      # The modal dialog is controlled by a different driver
-      # and the destroy action happens async
-      page.driver.browser.switch_to.alert.accept
-
-      visit "/admin/pages"
-      old_page = ContentPage.find_by_title attributes[:title]
-      expect(old_page).to be(nil)
-    end
-  end
-
-  describe "editing an existing draft" do
-    it "should apply changes to the existing draft and not create another one" do
-    end
   end
 end
