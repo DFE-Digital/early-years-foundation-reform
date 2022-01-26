5ba065c8716f773365a04e310e8a7c9c0d9db368
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-01-26 16:05:51 +0000

    add page name to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_142331) do
+ActiveRecord::Schema.define(version: 2022_01_26_155222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_142331) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "alt_text", default: "", null: false
+    t.string "page_name"
   end
 
   create_table "content_blocks", force: :cascade do |t|
