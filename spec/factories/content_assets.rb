602b604d6a468f334fffcb50c3737a2a28be1d9c
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-01-27 09:54:36 +0000

    updating specs to include page_name

--- spec/factories/content_assets.rb
+++ spec/factories/content_assets.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :content_asset do
     title { Faker::Lorem.sentence(word_count: 4).to_s }
     alt_text { Faker::Lorem.sentence(word_count: 4).to_s }
+    page_name { Faker::Lorem.sentence(word_count: 4).to_s }
     asset_file { Rack::Test::UploadedFile.new("spec/fixtures/files/sample.jpeg", "image/jpeg") }
   end
 end
