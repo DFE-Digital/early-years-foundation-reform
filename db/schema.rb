215debd21a17fe1ba79cc39d87db3bfc55f10032
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-02-01 16:20:13 +0000

    remove unused page_name column from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_145322) do
+ActiveRecord::Schema.define(version: 2022_02_01_161815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2022_02_01_145322) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "alt_text", default: "", null: false
-    t.string "page_name"
     t.integer "content_page_id"
   end
 
