d718e29162b02bcb3e0116802fab85c74b140ab9
borimogunje@and.digital <borimogunje@and.digital>
2022-02-07 16:02:01 +0000

    add content_list to factory bot

--- spec/factories/content_page_versions.rb
+++ spec/factories/content_page_versions.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :content_page_version do
     content_page # Note that another content page version will be created on creating this content page
     title { Faker::Commerce.product_name }
+    content_list { Faker::Lorem.paragraph }
     markdown { Faker::Lorem.paragraph }
     author { Faker::Name.name }
     description { Faker::Lorem.paragraph }
