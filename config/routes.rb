d10f7b1b0dd9255bf76d9399f7c9511e1a99223a
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-02-08 10:47:51 +0000

    adding rubocop changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get "/check" => "application#check"
-  
+
   # Note These have to be above the wildcard route
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
   get "/503", to: "errors#service_unavailable", via: :all
-  
+
   # explicit redirect for old link
-  get '/get-help-to-improve-your-practice/send-meeting-the-needs-of-all-children', to: redirect('/get-help-to-improve-your-practice/meeting-the-needs-of-all-children')
-  
+  get "/get-help-to-improve-your-practice/send-meeting-the-needs-of-all-children", to: redirect("/get-help-to-improve-your-practice/meeting-the-needs-of-all-children")
+
   resources :settings, only: %i[show create]
 
   constraints CmsRouteConstraint.new do
@@ -51,7 +51,6 @@ Rails.application.routes.draw do
 
   get "/:section/:slug", to: "content#show"
   get "/:slug", to: "content#show"
-
 
   root to: "content#index"
 end
