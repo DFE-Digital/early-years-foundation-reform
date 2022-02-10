6ddea21a3ed6ec15788940a28a0ab109dd407164
borimogunje@and.digital <borimogunje@and.digital>
2022-02-10 16:00:58 +0000

    fix rubocop issues

--- spec/features/content_page_versions_spec.rb
+++ spec/features/content_page_versions_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.feature "View pages", type: :feature do
   given(:parent_page) { FactoryBot.create(:content_page, :published, :top_level) }
-  given(:content_page_version) { FactoryBot.create(:content_page_version ) }
+  given(:content_page_version) { FactoryBot.create(:content_page_version) }
 
   scenario "Navigate to a preview of draft page and check that 2 print buttons are rendered on the page" do
     sign_in FactoryBot.create(:user)
     visit "/admin/pages/#{parent_page.id}/content_page_versions/#{content_page_version.id}/preview_of_draft"
-    page.find('p', text: 'Contents', id: 'contents-list-heading')
+    page.find("p", text: "Contents", id: "contents-list-heading")
 
-    expect(page.body).to have_button('Print this page').twice
+    expect(page.body).to have_button("Print this page").twice
   end
 
   scenario "Navigate to a preview of draft page and check that 1 print button is on the page" do
@@ -17,6 +17,6 @@ RSpec.feature "View pages", type: :feature do
     sign_in FactoryBot.create(:user)
     visit "/admin/pages/#{parent_page.id}/content_page_versions/#{page_version.id}/preview_of_draft"
 
-    expect(page.body).to have_button('Print this page').once
+    expect(page.body).to have_button("Print this page").once
   end
 end
--- spec/features/content_pages_spec.rb
+++ spec/features/content_pages_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "View pages", type: :feature do
   given(:parent_page) { FactoryBot.create(:content_page, :published, :top_level) }
   given(:child_page) { FactoryBot.create(:content_page, :published, parent_id: parent_page.id) }
-  given(:content_page_version) { FactoryBot.create(:content_page_version ) }
+  given(:content_page_version) { FactoryBot.create(:content_page_version) }
 
   scenario "Navigate to Content Pages" do
     sign_in FactoryBot.create(:user)
@@ -116,9 +116,9 @@ RSpec.feature "View pages", type: :feature do
   scenario "Navigate to a preview of live page and check that 2 print buttons are rendered on the page" do
     sign_in FactoryBot.create(:user)
     visit "/admin/pages/#{child_page.id}/preview_of_live"
-    page.find('p', text: 'Contents', id: 'contents-list-heading')
+    page.find("p", text: "Contents", id: "contents-list-heading")
 
-    expect(page.body).to have_button('Print this page').twice
+    expect(page.body).to have_button("Print this page").twice
   end
 
   scenario "Navigate to a preview of live page and check that 1 print button is on the page" do
@@ -126,6 +126,6 @@ RSpec.feature "View pages", type: :feature do
     sign_in FactoryBot.create(:user)
     visit "/admin/pages/#{content_page.id}/preview_of_live"
 
-    expect(page.body).to have_button('Print this page').once
+    expect(page.body).to have_button("Print this page").once
   end
 end
