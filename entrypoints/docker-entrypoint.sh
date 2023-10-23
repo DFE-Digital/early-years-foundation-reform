61454c5aa3eafe780046ba6033a1c6d940aedaba
Brett McHargue <ebrett@users.noreply.github.com>
2023-10-23 14:17:51 +0100

    Undo previous change to replant seed data

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -11,7 +11,7 @@ fi
 
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:prepare db:seed:replant
+bundle exec rake db:prepare db:seed
 
 # Start the application
 bundle exec rails s -b 0.0.0.0
