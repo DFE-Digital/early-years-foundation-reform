862a7aa651a5ec0f2f9337aaff91102432f8aa4b
Brett McHargue <ebrett@users.noreply.github.com>
2023-10-23 12:53:19 +0100

    Add env vars to profile

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -9,6 +9,8 @@ fi
 
 /usr/sbin/sshd
 
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:prepare db:seed:replant
 
 # Start the application
