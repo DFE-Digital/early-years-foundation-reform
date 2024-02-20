09b5aa3946cf8df0f3db981017526ff85483f5ad
Brett McHargue <ebrett@users.noreply.github.com>
2024-02-20 15:41:05 +0000

    Fix areas of learning pages

--- lib/upload.rb
+++ lib/upload.rb
@@ -71,7 +71,6 @@ class Upload
       )
       log_entry(entry)
     end
-<<<<<<< HEAD
 
     ContentBlock.all.find_each do |block|
       log "#{block.name} create"
@@ -82,8 +81,6 @@ class Upload
       )
       log_entry(block)
     end
-=======
->>>>>>> 6927ba1 (Upload script and additional changes)
   end
 
 private
