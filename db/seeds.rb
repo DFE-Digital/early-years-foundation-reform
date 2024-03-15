d82a90ff1f52fca4b1b6b966ebbca7f532f27385
Peter David Hamilton <pete@peterdavidhamilton.com>
2024-03-15 10:51:26 +0000

    Remove dibber

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-Seeder = Dibber::Seeder
-
-Seeder.seed(:user, name_method: :email) if Rails.application.credentials.user_password.present?
-
-Rails.logger.info 'Seeding completed'
-Seeder.report.each { |line| Rails.logger.info line }
