b6340db4e383511717558958de0a82c0fbbb7379
Katherine Martin <78093815+martikat@users.noreply.github.com>
2024-05-01 08:45:39 +0100

    WIP

---
+++ spec/system/page_tier_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+RSpec.describe 'First/Second/Third tier pages' do
+  describe 'First tier page' do
+    before do
+      visit '/areas-of-learning'
+    end
+
+    it 'has a heading' do
+      within '#hero-layout' do
+        expect(page).to have_text('Areas of learning')
+      end
+    end
+
+    it 'has a body' do
+      within '#hero-layout' do
+        expect(page.body).to be_present
+      end
+    end
+
+    it 'has a call to action component' do
+      within '.hf-ctas' do
+        expect(page).to have_text('Help to improve this website')
+      end
+    end
+
+    it 'has other resources' do
+      within '.hf-other' do
+        expect(page).to have_text('Other early years resources from the Department for Education')
+      end
+    end
+  end
+
+  describe 'Second tier page' do
+    before do
+      visit '/areas-of-learning/communication-and-language'
+    end
+
+    it 'has a heading' do
+      expect(page).to have_text('Overview')
+    end
+
+    # This currently renders govuk-heading-l for this h2 (does not match styling for third tier page)
+    xit 'renders h2 as govuk-heading-m' do
+      within '#communication-and-language-resources' do
+        expect(page).to include 'govuk-heading-m'
+      end
+    end
+
+    it 'has a body' do
+      expect(page.body).to be_present
+    end
+
+    it 'has a call to action component' do
+      within '.hf-ctas' do
+        expect(page).to have_text('Help to improve this website')
+      end
+    end
+
+    it 'has content footer component' do
+      within '.cta-banner' do
+        expect(page).to have_text('Early years child development training')
+      end
+    end
+  end
+
+  describe 'Third tier page' do
+    before do
+      visit '/areas-of-learning/communication-and-language/interactions'
+    end
+
+    it 'has a page title' do
+      expect(page.title).to eq('Help for early years providers : Interactions')
+    end
+
+    it 'has a contents list' do
+      within '.contents-on-page' do
+        expect(page).to have_text('Contents on this page')
+      end
+    end
+
+    it 'has a print this page button' do
+      expect(page).to have_text('Print this page')
+    end
+
+    it 'has a body' do
+      expect(page.body).to be_present
+    end
+
+    it 'renders h2 as govuk-heading-m' do
+      within '#why-interactions-are-important' do
+        expect(page.body).to include 'govuk-heading-m'
+      end
+    end
+
+    it 'has a back to top button' do
+      expect(page).to have_text('Back to top')
+    end
+
+    it 'has a call to action component' do
+      within '.hf-ctas' do
+        expect(page).to have_text('Help to improve this website')
+      end
+    end
+
+    it 'has content footer component' do
+      within '.cta-banner' do
+        expect(page).to have_text('Early years child development training')
+      end
+    end
+  end
+end
