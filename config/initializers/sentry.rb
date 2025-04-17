ef3fc8fa04bc7fd1fde0a0faea1acfaa6ba4e481
benvand <benjaminfvandersteen@gmail.com>
2025-04-17 14:04:45 +0100

    Were using enabled_environments wrong. A) until this PR merges there are no environments B) we want errors on all envs to appear in sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,6 @@
 Sentry.init do |config|
   config.dsn = Rails.application.config.sentry_dsn
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.enabled_environments = %w[production]
   config.environment = Rails.application.config.environment
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
