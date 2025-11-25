b8652171032751604c3e3de37a36523bc37b4897
RobertGHippo <robert.gammon@hippodigital.co.uk>
2025-11-25 15:34:44 +0000

    Use versionless certificate id

--- terraform-azure/terraform-azure-network/outputs.tf
+++ terraform-azure/terraform-azure-network/outputs.tf
@@ -40,7 +40,7 @@ output "kv_id" {
 
 output "kv_cert_secret_id" {
   description = "SSL certificate Secret ID"
-  value       = var.environment != "development" ? azurerm_key_vault_certificate.kv_cert[0].secret_id : null
+  value       = var.environment != "development" ? azurerm_key_vault_certificate.kv_cert[0].versionless_secret_id : null
 }
 
 output "kv_mi_id" {
