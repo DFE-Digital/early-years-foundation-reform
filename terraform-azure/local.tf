bbb5a58ca611f2157bd54624a663ba491651046b
martikat <78093815+martikat@users.noreply.github.com>
2026-07-30 10:36:07 +0100

    Update for bot token config

--- terraform-azure/local.tf
+++ terraform-azure/local.tf
@@ -17,6 +17,7 @@ locals {
     "CONTENTFUL_PREVIEW"                  = var.environment == "staging" ? "true" : null
     "ENVIRONMENT"                         = var.environment
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "BOT_TOKEN"                           = var.webapp_config_bot_token
     "DOMAIN"                              = var.webapp_config_domain
     "FEEDBACK_URL"                        = var.webapp_config_feedback_url
     "PRIVACY_POLICY_URL"                  = var.webapp_config_privacy_policy_url
@@ -44,6 +45,7 @@ locals {
     "CONTENTFUL_PREVIEW"                  = var.environment == "staging" ? "true" : null
     "ENVIRONMENT"                         = var.environment
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "BOT_TOKEN"                           = var.webapp_config_bot_token
     "DOMAIN"                              = var.webapp_config_domain
     "FEEDBACK_URL"                        = var.webapp_config_feedback_url
     "PRIVACY_POLICY_URL"                  = var.webapp_config_privacy_policy_url
